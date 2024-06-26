84ae5e2ae5ba13ac5b4b287d6139d26795c9a378
suaib <suaib8211@gmail.com>
2024-06-26 16:33:20 +0530

    backend depoly

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://user-management-api-x3x1.onrender.com/api';
 
 export const register = (userData) => axios.post(`${API_URL}/auth/register`, userData);
 
